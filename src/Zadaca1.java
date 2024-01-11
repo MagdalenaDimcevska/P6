265385aab533b27c067be773cd9a27b31d18cd16
MagdalenaDimcevska <147504217+MagdalenaDimcevska@users.noreply.github.com>
2024-01-11 23:02:58 +0100

    Add first java project

---
+++ src/Zadaca1.java
@@ -0,0 +1,22 @@
+public class Zadaca1 {
+	public static void main(String[]args) {
+		        int a = 3; 
+		        int b = 10; 
+		        int c = 7; 
+		        int k, d;
+
+		        if (a <= 5 && b > (a - c)) {
+		            k = 1;
+		        } else {
+		        	k=0;
+		        }
+
+		        if (c == 0) {
+		            d = 0;
+		        } else {
+		        	d=1;
+		        }
+		        System.out.println("k = " + k);
+		        System.out.println("d = " + d);
+		    }
+		}
